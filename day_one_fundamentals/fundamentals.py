95a8c0c138db7f493f2970ced0022b5c559afc92
Alaa-1 <aladdinabidi@gmail.com>
2022-07-19 08:46:37 +0100

    Added python track folders

---
+++ day_one_fundamentals/fundamentals.py
@@ -0,0 +1,32 @@
+'''
+# ! Python Fundatmentals
+
+# TODO: Syntax
+#       * Indentation
+#       * Block of code
+# TODO: Data and Composite Types
+#       * Integers
+#       * Strings
+#       * Booleans
+#       * Lists
+#       * Dictionaries
+#       * Tuples
+# TODO: Conditionals
+#       * if, elif, else
+#       * chaining conditional statements
+# TODO: Loops
+#       * for loops
+#       * using with range
+#       * using with composite types
+#TODO: Built In Methods
+#       * print()
+#       * len()
+#       * conversions: str(), int()
+#       * range()
+# TODO: Functions
+#       * parameters
+#       * arguments
+#       * return vs print
+'''
+
+
