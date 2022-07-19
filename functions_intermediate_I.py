7e45847cb64b02eb4c5024df084bf7562c6faafe
Alaa-1 <aladdinabidi@gmail.com>
2022-07-19 10:56:18 +0100

    functions intermediate 1

---
+++ functions_intermediate_I.py
@@ -0,0 +1,98 @@
+# 1. Update Values in Dictionaries and Lists
+
+x = [[5, 2, 3], [10, 8, 9]]
+x[1][0] = 15
+# print(x)
+
+students = [
+    {"first_name": "Michael", "last_name": "Jordan"},
+    {"first_name": "John", "last_name": "Rosales"},
+]
+
+students[0]["last_name"] = "Bryant"
+
+# print(students)
+sports_directory = {
+    "basketball": ["Kobe", "Jordan", "James", "Curry"],
+    "soccer": ["Messi", "Ronaldo", "Rooney"],
+}
+
+sports_directory["soccer"][0] = "Andres"
+# print(sports_directory)
+
+z = [{"x": 10, "y": 20}]
+z[0]["y"] = 30
+# print(z)
+
+# 2. Iterate Through a List of Dictionaries
+students = [
+    {"first_name": "Michael", "last_name": "Jordan"},
+    {"first_name": "John", "last_name": "Rosales"},
+    {"first_name": "Mark", "last_name": "Guillen"},
+    {"first_name": "KB", "last_name": "Tonel"},
+]
+
+
+def iterateDictionary(students):
+    for i in students:
+        print(f"first name - {i['first_name']}, last name - {i['last_name']}")
+
+
+# iterateDictionary(students)
+
+
+# 3. Get Values From a List of Dictionaries
+
+
+def iterateDictionary2(key_name, some_list):
+    for item in some_list:
+        if key_name in item.keys():
+            print(item[key_name])
+
+
+# iterateDictionary2("first_name", students)
+# iterateDictionary2("last_name", students)
+
+# 4. Iterate Through a Dictionary with List Values
+
+
+def printInfo(some_dict):
+    for key, value in some_dict.items():
+        print(len(value), key)
+        for element in value:
+            print(element)
+
+
+dojo = {
+    "locations": ["San Jose", "Seattle", "Dallas", "Chicago", "Tulsa", "DC", "Burbank"],
+    "instructors": [
+        "Michael",
+        "Amy",
+        "Eduardo",
+        "Josh",
+        "Graham",
+        "Patrick",
+        "Minh",
+        "Devon",
+    ],
+}
+printInfo(dojo)
+"""# output:
+7 LOCATIONS
+San Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+    
+8 INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devon"""
